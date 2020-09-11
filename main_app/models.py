7ba283e0b37daf4e77ab86105d4cb696c573e93e
unknown <draz@ualberta.ca>
2020-09-11 14:46:26 -0600

    final assessment code

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Widget(models.Model):
+    description = models.CharField(max_length=100)
+    time = models.IntegerField()
+    person = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '%s' % (self.description)
--- main_app/views.py
+++ main_app/views.py
@@ -2,10 +2,38 @@ from django.shortcuts import render
 
 # Add the following import
 from django.http import HttpResponse
-
+from django.views.generic.edit import CreateView, DeleteView
+from .models import Widget
+from .forms import WidgetForm
 # Define the home view
 
 
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    widget = Widget.objects.all
+    form = WidgetForm()
+    return render(request, 'index.html', {'form': form, 'widget': widget})
 # Create your views here.
+
+
+class WidgetCreate(CreateView):
+    model = Widget
+    fields = '__all__'
+    success_url = '/'
+
+
+def add_widget(request, widget_id):
+    # create the ModelForm using the data in request.POST
+    form = WidgetForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the cat_id assigned
+        new_widget = form.save(commit=False)
+        new_widget.widget_id = widget_id
+        new_widget.save()
+    return redirect('index', {'form': form})
+
+
+class WidgetDelete(DeleteView):
+    model = Widget
+    success_url = '/'
